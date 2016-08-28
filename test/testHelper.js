6d1dddf0a766dc051e1a71d6b6c6f26e9e5a58dd
David Rodriguez Fuentes <davidrf.developer@gmail.com>
2016-08-28 14:20:42 -0400

    Update test suite

--- test/testHelper.js
+++ test/testHelper.js
@@ -1,30 +1,25 @@
-// test/testHelper.js
 import { shallow, mount } from 'enzyme';
-import 'jasmine-ajax';
 import jasmineEnzyme from 'jasmine-enzyme';
 import React from 'react';
+import { browserHistory } from 'react-router';
+import { push, syncHistoryWithStore } from 'react-router-redux';
 
 Object.assign(global, {
-  jasmineEnzyme,
+  browserHistory,
   mount,
+  push,
   React,
-  shallow
+  shallow,
+  syncHistoryWithStore
 });
 
 beforeEach(() => {
   jasmineEnzyme();
 });
 
-// function to require all modules for a given context
-let requireAll = requireContext => {
-  requireContext.keys().forEach(requireContext);
-};
-
 // require all js files that end with Spec.js or Spec.jsx in the test folder
-requireAll(require.context('./', true, /Spec.jsx?$/));
-
-// require all js files except main.js in the src folder
-requireAll(require.context('../src/', true, /^((?!main).)*\.jsx?$/));
+var testsContext = require.context(".", true, /Spec.jsx?$/);
+testsContext.keys().forEach(testsContext);
 
 // output to the browser's console when the tests run
 console.info(`TESTS RAN AT ${new Date().toLocaleTimeString()}`);
